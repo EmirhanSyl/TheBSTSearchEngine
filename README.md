b630cf46ca1b05009f4af8bc90422d5d96aa4145
Emirhan Soylu <61618968+EmirhanSyl@users.noreply.github.com>
2024-05-01 16:38:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TheBSTSearchEngine
+ Mini desktop search engine with Binary Search Tree
---
+++ src/main/java/com/thebst/thebstsearchengine/TheBSTSearchEngine.java
@@ -0,0 +1,12 @@
+package com.thebst.thebstsearchengine;
+
+/**
+ *
+ * @author thebst
+ */
+public class TheBSTSearchEngine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
